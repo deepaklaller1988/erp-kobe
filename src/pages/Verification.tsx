a8d34bd4d25c4d48c1df32773ad65d4af480b5d6
dinesh <dlaller1988@gmail.com>
2024-12-13 16:13:44 +0530

    verification done

--- src/pages/Verification.tsx
+++ src/pages/Verification.tsx
@@ -3,9 +3,24 @@ import Password from "../components/Password";
 import Successmessage from "../components/Successmessage";
 import { useLocation } from "react-router-dom";
 import API from "../utils/API";
+import MiniLoader from "../components/MiniLoader";
+import { toast } from "react-toastify";
 
 const Verification = () => {
   const [type, setType] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+  const showToast = (type: "success" | "error" | "warn", message: string) => {
+    toast[type](message, {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+    });
+  };
   const location = useLocation();
 
   useEffect(() => {
@@ -18,13 +33,24 @@ const Verification = () => {
     const type: string | null = getQueryParam("type");
 
     if (token && type) {
-      API.get(`auth/account-activation?token=${token}`)
+      setIsLoading(true);
+      API.get(`auth/account-activation?token=${token}&type=${type}`)
         .then((response) => {
-          console.log(response);
+
+          if(response?.error?.code){
+        showToast("error", "Account not verified");
+            setError("ERR_ACCOUNT_NOT_VERIFIED");
+          }
+          if(response?.success){
+            // showToast("success","Password updated Successfully")
+            setError("");
+          }
         })
         .catch((error) => {
-          console.error("Error during account activation:", error);
+          setError("Error during verifying link")
+          console.error("Error during verifying link", error);
         });
+      setIsLoading(false);
     }
 
     setType(type);
@@ -32,7 +58,14 @@ const Verification = () => {
 
   return (
     <>
-      {type === "account-activation" ? <Successmessage /> : <Password />}
+      {isLoading ? <div className="mt-4 flex w-full"><MiniLoader /></div> :
+      error ? 
+      <div className="flex items-center justify-center w-full h-screen">
+        <p className="text-red-500 text-4xl">{error === "ERR_ACCOUNT_NOT_VERIFIED" ? "Please activate your account first" : error}</p>
+      </div>
+      :
+        type === "account-activation" ? <Successmessage /> : <Password />
+      }
       
     </>
   );
